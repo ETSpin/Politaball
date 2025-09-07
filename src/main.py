78bcb4dfd149b17bcdee8111bea3cd1de39e3db4
Ed Spin <ets3480@gmail.com>
2025-09-07 14:27:42 +0200

    Initial commit: starter template scaffold

---
+++ src/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+File: {file_name}
+Author: MORS
+Date: {date}
+
+Description:
+{brief_description_of_the_file}
+
+Usage:
+{brief_usage_instructions}
+
+"""
+
+
+def main():
+    """
+    TBD: Method DocString
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
